7a54212de8c981ebc22d2043e3b12772195abf03
Jhonathan Mangueira Gimenes <jhonathan.gimenes@tre-to.jus.br>
2019-05-07 18:15:51 -0300

    inclusão do formulário de consulta

--- src/components/Animation.vue
+++ src/components/Animation.vue
@@ -4,34 +4,46 @@
             <h1 class="display-4">Animantion Vue!</h1>
         </div>
         <div class="container">
-            <div>
-                <button class="btn btn-primary mb-4" @click.prevent="mostrar = !mostrar">Mudar</button>
-                <transition appear name="fade">
-                    <div class="alert alert-primary" v-if="mostrar"> Animação</div>
-                </transition>
-            </div>
-             <div>
-                <button class="btn btn-danger mt-4 mb-4" @click.prevent="mostrar2 = !mostrar2">Mudar</button>
-                <transition appear name="zoom">
-                    <div class="alert alert-danger" v-if="mostrar2"> Animação</div>
-                </transition>
-            </div>
-            <div>
-                <button class="btn btn-success mt-4 mb-4" @click.prevent="mostrar3 = !mostrar3">Mudar</button>
-                <transition appear name="slide">
-                    <div class="alert alert-success" v-if="mostrar3"> Animação</div>
-                </transition>
-            </div>
-            <div>
-                <button class="btn btn-warning mt-4 mb-4" @click.prevent="mostrar4 = !mostrar4">Mudar</button>
-                <transition 
-                appear
-                    appear-active-class="animated flash"
-                    enter-active-class="animated bounceIn"
-                    leave-active-class="animated bounceOut"
-                >
-                    <div class="alert alert-warning" v-if="mostrar4"> Animação</div>
-                </transition>
+            <div class="row">
+                <div class="col-md-6">
+                    <form @submit.prevent="mark">
+                        <label>Paciente</label>
+                        <input class="form-control"
+                            type="text"
+                            placeholder="Nome paciente"
+                            v-model.lazy.trim="patient.name">
+                        <label>Idade</label>
+                        <input class="form-control"
+                            type="number"
+                            placeholder="Idade do paciente"
+                            v-model="patient.age">
+                        <label>Horario do atendimento</label>
+                        <select class="form-control" v-model="patient.schedule">
+                            <option selected disabled value="">Selecione horario...</option>
+                            <option value="9:00">9:00</option>
+                            <option value="10:00">10:00</option>
+                            <option value="11:00">11:00</option>
+                            <option value="12:00">12:00</option>
+                        </select>
+                        <div class="float-right mt-3">
+                            <button class="btn btn-secondary mr-2">Cancelar</button>
+                            <button class="btn btn-success">Salvar</button>
+                        </div>
+                    </form>
+                </div>
+                <div class="col-md-6">
+                    <transition-group name="list" 
+                        class="list-group" tag="ul">
+                        <li class="list-group-item"
+                            v-for="consultation in consultations" 
+                            :key="consultation.id">
+                            <strong>Paciente:</strong> {{ consultation.name }}
+                            <strong>Idade:</strong> {{ consultation.age }}
+                            <strong>Horario:</strong> {{ consultation.schedule }}
+                            <button class="btn btn-danger float-right" @click="remove(consultation)">X</button>
+                        </li>
+                    </transition-group>
+                </div>
             </div>
         </div>
     </div>
@@ -40,66 +52,49 @@
 export default {
     data() {
         return {
-            mostrar: true,
-            mostrar2: true,
-            mostrar3: true,
-            mostrar4: true
+            patient: {
+                id: 0,
+                name: '',
+                age: undefined,
+                schedule: ''
+            },
+            consultations: [
+                {id: 1, name: 'João', age: '38', schedule: '9:00'},
+                {id: 2, name: 'Pedro', age: '22', schedule: '10:00'},
+                {id: 3, name: 'Maria', age: '59', schedule: '11:00'},
+                {id: 4, name: 'Rafael', age: '13', schedule: '12:00'}
+            ]
+        }
+    },
+    methods: {
+        mark() {
+            this.patient.id = +this.consultations.length + 1
+            this.consultations.push(this.patient)
+            this.patient = {}
+        },
+        remove (consultation) {
+            let index = this.consultations.indexOf(consultation);
+            this.consultations.splice(index, 1)
         }
     }
 }
 </script>
 
 <style scoped>
-
-    .fade-enter, .fade-leave-to  {
+    .list-enter, .list-leave-to {
         opacity: 0;
+        transform: translateX(-70px)
+    }
+    .list-enter-active, .list-leave-active {
+        transition: all 0.5s;
     }
 
-    .fade-enter-active, .fade-leave-active {
-        transition: opacity 1s;
+    .list-move {
+        transition: all 0.5s;
     }
 
-    .fade-enter-to, .fade-leave {
-        opacity: 1;
+    .list-leave-active {
+        position: absolute;
+        width: calc(100% - 31px);
     }
-
-    .zoom-enter, .zoom-leave-to {
-        transform: scale(0);
-    }
-
-    .zoom-enter-active, .zoom-leave-active {
-        transition: transform 0.5s;
-    }
-
-    .zoom-to-active, .zoom-leave {
-        transform: scale(1);
-    }
-
-    .slide-enter, .slide-leave-to {
-        opacity: 0;
-    }
-
-    .slide-enter-active {
-        animation: slides 0.7s;
-        transition: opacity 0.7s;
-    }
-
-    .slide-enter-to, .slide-leave {
-        opacity: 1;
-    }
-
-    .slide-leave-active {
-        animation: slides 0.7s reverse;
-        transition: opacity 0.7s;
-    }
-
-    @keyframes slides {
-        0% {
-            transform: translateX(-100px);
-        }
-        100% {
-            transform: translateX(0px);
-        }
-    }
-
 </style>
