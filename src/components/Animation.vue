e8fba2a0749a4eda18ef299928d8fb22e137b385
unknown <gimenesjhonathan@gmail.com>
2019-05-06 21:14:00 -0300

    inclusão da animation.css e novas animações

--- src/components/Animation.vue
+++ src/components/Animation.vue
@@ -4,20 +4,35 @@
             <h1 class="display-4">Animantion Vue!</h1>
         </div>
         <div class="container">
-            <button class="btn btn-primary mb-4" @click.prevent="mostrar = !mostrar">Mudar</button>
-            <transition name="fade">
-                <div class="alert alert-primary" v-if="mostrar"> Animação</div>
-            </transition>
-
-            <button class="btn btn-danger mt-4 mb-4" @click.prevent="mostrar2 = !mostrar2">Mudar</button>
-            <transition name="zoom">
-                <div class="alert alert-danger" v-if="mostrar2"> Animação</div>
-            </transition>
-
-            <button class="btn btn-success mt-4 mb-4" @click.prevent="mostrar3 = !mostrar3">Mudar</button>
-            <transition name="slide">
-                <div class="alert alert-success" v-if="mostrar3"> Animação</div>
-            </transition>
+            <div>
+                <button class="btn btn-primary mb-4" @click.prevent="mostrar = !mostrar">Mudar</button>
+                <transition appear name="fade">
+                    <div class="alert alert-primary" v-if="mostrar"> Animação</div>
+                </transition>
+            </div>
+             <div>
+                <button class="btn btn-danger mt-4 mb-4" @click.prevent="mostrar2 = !mostrar2">Mudar</button>
+                <transition appear name="zoom">
+                    <div class="alert alert-danger" v-if="mostrar2"> Animação</div>
+                </transition>
+            </div>
+            <div>
+                <button class="btn btn-success mt-4 mb-4" @click.prevent="mostrar3 = !mostrar3">Mudar</button>
+                <transition appear name="slide">
+                    <div class="alert alert-success" v-if="mostrar3"> Animação</div>
+                </transition>
+            </div>
+            <div>
+                <button class="btn btn-warning mt-4 mb-4" @click.prevent="mostrar4 = !mostrar4">Mudar</button>
+                <transition 
+                appear
+                    appear-active-class="animated flash"
+                    enter-active-class="animated bounceIn"
+                    leave-active-class="animated bounceOut"
+                >
+                    <div class="alert alert-warning" v-if="mostrar4"> Animação</div>
+                </transition>
+            </div>
         </div>
     </div>
 </template>
@@ -28,6 +43,7 @@ export default {
             mostrar: true,
             mostrar2: true,
             mostrar3: true,
+            mostrar4: true
         }
     }
 }
@@ -59,30 +75,22 @@ export default {
         transform: scale(1);
     }
 
-    .slide-enter {
+    .slide-enter, .slide-leave-to {
         opacity: 0;
     }
 
     .slide-enter-active {
         animation: slides 0.7s;
-        transition: opacity 0.9s;
+        transition: opacity 0.7s;
     }
 
-    .slide-enter-to {
-        opacity: 1;
-    }
-
-    .slide-leave {
+    .slide-enter-to, .slide-leave {
         opacity: 1;
     }
 
     .slide-leave-active {
         animation: slides 0.7s reverse;
         transition: opacity 0.7s;
-    }
-
-    .slide-leave-to {
-        opacity: 0;
     }
 
     @keyframes slides {
